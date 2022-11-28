d3c0ec7c692f091cf872fe48e2ce8557c8c98767
NAGARRO\yashsingh01 <yash.singh01@nagarro.com>
2022-11-29 02:47:15 +0530

    adding webapp files

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -12,22 +12,22 @@ import java.sql.Statement;
 @RestController  
 public class HelloWorldController   
 {  
-    @RequestMapping("/")  
+    @RequestMapping("/api/name")  
     public String hello()   
     {  
-        String url = "jdbc:postgresql://azure-bench-server.postgres.database.azure.com:5432/postgres?user=Nagarro_Yash&password=Happy9810741638&sslmode=require";
+        // String url = "jdbc:postgresql://azure-bench-server.postgres.database.azure.com:5432/postgres?user=Nagarro_Yash&password=Happy9810741638&sslmode=require";
 
-        try (Connection con = DriverManager.getConnection(url);
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery("SELECT name FROM LuckyName where Name='Yash'")) {
+        // try (Connection con = DriverManager.getConnection(url);
+        //         Statement st = con.createStatement();
+        //         ResultSet rs = st.executeQuery("SELECT name FROM LuckyName where Name='Yash'")) {
 
-            if (rs.next()) {
-                return rs.getString(1);
-            }
+        //     if (rs.next()) {
+        //         return rs.getString(1);
+        //     }
 
-        } catch (SQLException ex) {
-            return ex.toString();
-        }
+        // } catch (SQLException ex) {
+        //     return ex.toString();
+        // }
         return "dummy_string";
     }  
 }  
